4c42466e444d82a31687e8918714c89fdc65b6a5
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2025-01-28 01:36:02 +0530

    Spring Boot version update

--- src/main/java/com/javatechie/spring/cache/api/model/User.java
+++ src/main/java/com/javatechie/spring/cache/api/model/User.java
@@ -6,14 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
 
 @Data
-@FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -23,9 +20,9 @@ public class User implements Serializable
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	Long id;
-	String name;
-	String address;
+	private Long id;
+	private String name;
+	private String address;
 
 	public User(String name, String address)
 	{
